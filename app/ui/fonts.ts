c740141f7d25df7a75a62302b81cb367a18b5547
Darcy12138 <94740166+Darcy12138@users.noreply.github.com>
2025-05-12 16:58:56 +0800

    第一次提交

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import {Inter, Playwrite_DK_Loopet} from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+
+export const lusitana = Playwrite_DK_Loopet({
+    weight: ['400'],
+});
